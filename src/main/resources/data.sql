55a1746dfc060ccd5cac97b9449c1a45a4a1a310
lucia <luciasegrelles@gmail.com>
2025-03-27 21:25:09 +0100

    bases de datos compleats

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO zone(name) VALUES 
+('FRONTIER TOWN'), 
+('CAMP SNOOPY™'),
+('GEMINI MIDWAY'), 
+('FRONTIER TRAIL'),
+('MILLENNIUM MIDWAY'), 
+('MAIN MIDWAY'), 
+('PLANET SNOOPY™'),
+('KIDDY KINGDOM'),
+('LAKESIDE MIDWAY');
+
+
+INSERT INTO thrill (name) VALUES 
+('Low'), 
+('Moderate'), 
+('Aggressive'), 
+('Mild'), 
+('High');
+
+
+INSERT INTO type (name) VALUES 
+('Roller coasters'), 
+('Thrill rides'), 
+('Family rides'), 
+('Water Ride'), 
+('Children rides');
