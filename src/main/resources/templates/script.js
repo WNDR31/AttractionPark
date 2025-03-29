f8bf07d620ecc4395d00cc1a683085224e86de3f
crisssrg7 <crg2151@gmail.com>
2025-03-29 12:46:46 +0100

    Modificado compra, .js y .css

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -1,16 +1,29 @@
 
-function mostrarVentanaCompra(titulo, descripcion) {
-    document.getElementById("modalTitulo").textContent = titulo;
-    document.getElementById("modalDescripcion").textContent = descripcion;
+//Función para mostrar le ventada de compra (modal)
+function mostrarVentanaCompra(titulo) {
     
     let modal = new bootstrap.Modal(document.getElementById("modalCompra"));
     modal.show();
 }
 
+
+//Funciones para añadir al carrito
 document.getElementById('agregarCarrito').addEventListener('click', function () {
     const tipoEntrada = document.getElementById('tipoEntrada').value;
     const cantidad = document.getElementById('cantidad').value;
     const tipoEntradaText = document.getElementById('tipoEntrada').options[document.getElementById('tipoEntrada').selectedIndex].text;
+
+    if (entradasSeleccionadas.has(tipoEntrada)) {
+        alert('Este tipo de entrada ya ha sido añadido al carrito.');
+        return;
+    }
+
+    if (tipoEntrada === "" || cantidad === "") {
+        alert("Opciones incorrectas.");
+        return;
+    }
+
+    entradasSeleccionadas.add(tipoEntrada);
 
     const carrito = document.getElementById('carrito');
     const li = document.createElement('li');
@@ -22,8 +35,14 @@ document.getElementById('agregarCarrito').addEventListener('click', function () 
     removeButton.textContent = 'Eliminar';
     removeButton.addEventListener('click', function () {
         carrito.removeChild(li);
+        entradasSeleccionadas.delete(tipoEntrada);
     });
 
     li.appendChild(removeButton);
     carrito.appendChild(li);
 });
+
+const entradasSeleccionadas = new Set();
+
+
+
