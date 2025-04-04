41df65e4b194cfce0702981bae20cbf23d861073
crisssrg7 <crg2151@gmail.com>
2025-04-04 18:28:17 +0200

    TODO JUNTO

--- src/main/java/com/example/demo/controller/AttractionsController.java
+++ src/main/java/com/example/demo/controller/AttractionsController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Controller
--- src/main/java/com/example/demo/services/ExportXmlService.java
+++ src/main/java/com/example/demo/services/ExportXmlService.java
@@ -13,7 +13,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Service
 public class ExportXmlService {
